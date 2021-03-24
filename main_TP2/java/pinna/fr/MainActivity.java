0ee62aca32bd2bfc2eb3bfba9f7725659b082c77
Bafrac <48585244+Bafrac@users.noreply.github.com>
2021-03-24 21:11:55 +0100

    Add files via upload

---
+++ main_TP2/java/pinna/fr/MainActivity.java
@@ -0,0 +1,134 @@
+package pinna.fr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button envoyer = null;
+    Button reset = null;
+    EditText taille = null;
+    EditText poids = null;
+    CheckBox commentaire = null;
+    RadioGroup group = null;
+    TextView result = null;
+    private final String texteInit = "Cliquez sur le bouton « Calculer l'IMC » pour obtenir un résultat.";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // On récupère toutes les vues dont on a besoin
+        envoyer = (Button) findViewById(R.id.calcul);
+        reset = (Button) findViewById(R.id.reset);
+        taille = (EditText) findViewById(R.id.taille);
+        poids = (EditText) findViewById(R.id.poids);
+        commentaire = (CheckBox) findViewById(R.id.commentaire);
+        group = (RadioGroup) findViewById(R.id.group);
+        result = (TextView) findViewById(R.id.result);
+
+        // On attribue un listener adapté aux vues qui en ont besoin
+        envoyer.setOnClickListener(envoyerListener);
+        reset.setOnClickListener(resetListener);
+        commentaire.setOnClickListener(checkedListener);
+        taille.addTextChangedListener(textWatcher);
+        poids.addTextChangedListener(textWatcher);
+    }
+
+    private View.OnClickListener envoyerListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            //  on récupère la taille
+            String t = taille.getText().toString();
+            // On récupère le poids
+            String p = poids.getText().toString();
+            float tValue = Float.valueOf(t);
+
+            // Puis on vérifie que la taille est cohérente
+            if (tValue <= 0)
+                Toast.makeText(MainActivity.this, "La taille doit être positive", Toast.LENGTH_SHORT).show();
+            else {
+                float pValue = Float.valueOf(p);
+                if (pValue <= 0)
+                    Toast.makeText(MainActivity.this, "Le poids doit etre positif", Toast.LENGTH_SHORT).show();
+                else {
+                    // Si l'utilisateur a indiqué que la taille était en centimètres
+                    // On vérifie que la Checkbox sélectionnée est la deuxième à l'aide de son identifiant
+                    if (group.getCheckedRadioButtonId() == R.id.radio_centimetre)
+                        tValue = tValue / 100;
+                    float imc = pValue / (tValue * tValue);
+                    String appreciation = interpreteIMC(imc);
+                    String resultat = "Votre IMC est " + imc + " "+ appreciation + " . ";
+
+                    result.setText(resultat);
+                }
+            }
+        }
+    };
+
+    private View.OnClickListener resetListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            poids.getText().clear();
+            taille.getText().clear();
+            result.setText(texteInit);
+        }
+    };
+
+    private View.OnClickListener checkedListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            if (((CheckBox) v).isChecked()) {
+                result.setText(texteInit);
+            }
+        }
+    };
+
+    private TextWatcher textWatcher = new TextWatcher() {
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            result.setText(texteInit);
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            if(s.toString().contains(".")){
+                RadioButton radio_metre = findViewById(R.id.radio_metre);
+                radio_metre.setChecked(true);
+            }
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {}
+    };
+
+    public String interpreteIMC(float imc){
+        String appreciation = "";
+        if(imc < 16.5) appreciation = "famine";
+        else if(imc < 18.5) appreciation = getString(R.string.maigre);
+        else if(imc < 25) appreciation = getString(R.string.normal);
+        else if(imc < 30) appreciation = getString(R.string.obesite_modere);
+        else if(imc < 35) appreciation = getString(R.string.maigre);
+        else if(imc < 40) appreciation = getString(R.string.tgros);
+        else if(imc > 40) appreciation = getString(R.string.ttgros);
+        return appreciation;
+    }
+    
+}
